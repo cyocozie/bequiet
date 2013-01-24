df85c63f73d2c877c7ed68292b92c79cac775506
cyocozie <hottodayok@naver.com>
2013-01-24 18:37:13 +0900

    testtest

--- src/com/project/bequiet/ListActivity.java
+++ src/com/project/bequiet/ListActivity.java
@@ -10,6 +10,7 @@ public class ListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
+		int a;
 	}
 
 	@Override
